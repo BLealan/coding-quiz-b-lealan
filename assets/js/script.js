63acb19195651b54a3c9ac1d10e4712fbb3f03d8
BLealan <143086275+BLealan@users.noreply.github.com>
2023-10-20 11:13:57 +0100

    code break down

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+
+// user clicks start button
+// listen event for click of start button
+
+// THEN a timer starts and I am presented with a question
+
+//timer starts at 60s
+// timed event displayed start at 60s
+//first question displayed
+// add question text to HTML
+// add answer options to HTML
+
+// WHEN I answer a question
+
+// user clicks on correct answer
+// answer colour changes to green
+
+// THEN I am presented with another question
+
+// next question is disaplyed in HTML
+
+// WHEN I answer a question incorrectly
+
+// user selects incorrect answer
+// answer stays as red
+
+// THEN time is subtracted from the clock
+// check if answer is incorrect 
+// -5 s on clock
+
+// WHEN all questions are answered or the timer reaches 0
+
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// user input for initials
+// save score and initials to local server
