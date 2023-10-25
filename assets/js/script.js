eebff458effdb39345b6d612a7fa7e07b48ef1e3
BLealan <143086275+BLealan@users.noreply.github.com>
2023-10-25 12:10:51 +0100

    question functions declared

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,33 +44,61 @@ var countdownMessage = document.querySelector(".countdown-text");
 var startButton = document.querySelector(".start-button");
 var questionEl = document.querySelector("#question");
 var score = document.querySelector(".score");
-var answerOne = document.querySelector(".guess0");
-var answerTwo = document.querySelector(".guess1");
-var answerThree = document.querySelector(".guess2");
-var answerFour = document.querySelector(".guess3");
+var choiceOne = document.querySelector("#guess0");
+var choiceTwo = document.querySelector("#guess1");
+var choiceThree = document.querySelector("#guess2");
+var choiceFour = document.querySelector("#guess3");
 var progress = document.querySelector(".progress");
 
 var listOfQuestions = [
     "In JavaScript how do you declare a variable?",
     "What is an array?",
-    "Which comparitve operator equates to values AND data not being equal?"];
-var answers1 = ["var =", "var:", "var -", "var()"];
-var answers2 = ["A way to campare values", "A list of data", "Inline styling", "An aqautic creature"];
-var answers3 = [];
-var answers4 = [];
+    "Which comparitve operator equates to values AND data not being equal?",
+    "What are the two parts to an object property?"];
+var answer1 = ["var =", "var:", "var -", "var()"];
+var correctAnswer = "var =";
+var answer2 = ["A way to campare values", "A list of data", "Inline styling", "An aqautic creature"];
+var answer3 = ["!==", "!", "¡!", "=!="];
+var answer4 = ["data:input", "name:code", "key:value", "content:expression"];
 var isTrue
 var isFlase
 var gameOverWin
 var gameOverLose
 
+var countdownTimer = 0;
 var nameInitials
 var saveScore //will be time remaining
 var previousScore
 
-function displayQuestion(){
+function displayQuestion1(){
     questionEl.textContent = listOfQuestions[0];
+    choiceOne.textContent = answer1[0];
+    choiceTwo.textContent = answer1[1];
+    choiceThree.textContent = answer1[2];
+    choiceFour.textContent = answer1[3];
 }
 
+function displayQuestion2(){
+    questionEl.textContent = listOfQuestions[1];
+    choiceOne.textContent = answer2[0];
+    choiceTwo.textContent = answer2[1];
+    choiceThree.textContent = answer2[2];
+    choiceFour.textContent = answer2[3];
+}
+function displayQuestion3(){
+    questionEl.textContent = listOfQuestions[2];
+    choiceOne.textContent = answer2[0];
+    choiceTwo.textContent = answer3[1];
+    choiceThree.textContent = answer3[2];
+    choiceFour.textContent = answer3[3];
+}
+function displayQuestion4(){
+    questionEl.textContent = listOfQuestions[3];
+    choiceOne.textContent = answer4[0];
+    choiceTwo.textContent = answer4[1];
+    choiceThree.textContent = answer4[2];
+    choiceFour.textContent = answer4[3];
+}
 startButton.addEventListener("click", function(){
     countdownTimer = 60;
     var timer = setInterval(function(){
@@ -83,7 +111,7 @@ startButton.addEventListener("click", function(){
     }
 
     },1000);
+
 });
 
-startButton.addEventListener("click", displayQuestion);
-
+startButton.addEventListener("click", displayQuestion1);
